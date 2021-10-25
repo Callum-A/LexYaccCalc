58dc58fbbf45fff870846eaa6a05c0106b8360a8
Callum Anderson <callumanderson745@gmail.com>
2021-10-25 09:54:51 +0100

    Clean up ev func

--- src/evaluator.c
+++ src/evaluator.c
@@ -1,41 +1,20 @@
 #include "evaluator.h"
 #include <stdio.h>
 
+double evBinaryOp(BinaryOp *binaryOp);
+double evUnaryOp(UnaryOp *unaryOp);
+void evProgram(Program *program);
+
 double ev(Node *root) {
     switch (root->nodeType) {
     case BINARY_OP:
-        switch (root->as.binaryOp->op) {
-        case '+':
-            return ev(root->as.binaryOp->left) + ev(root->as.binaryOp->right);
-        case '-':
-            return ev(root->as.binaryOp->left) - ev(root->as.binaryOp->right);
-        case '/':
-            return ev(root->as.binaryOp->left) / ev(root->as.binaryOp->right);
-        case '*':
-            return ev(root->as.binaryOp->left) * ev(root->as.binaryOp->right);
-        default:
-            // Unreachable
-            printf("UNRECOGNISED BINARY OP\n");
-            return 0;
-        }
-        break;
+        return evBinaryOp(root->as.binaryOp);
     case UNARY_OP:
-        switch (root->as.unaryOp->op) {
-        case '-':
-            return -ev(root->as.unaryOp->right);
-        default:
-            // Unreachable
-            printf("Unrecognised UNARY OP\n");
-            return 0;
-        }
-        break;
+        return evUnaryOp(root->as.unaryOp);
     case NUM:
         return root->as.number->value;
     case PROGRAM: {
-        int len = root->as.program->stmts->count;
-        for (int i = 0; i < len; i++) {
-            ev(&root->as.program->stmts->nodes[i]);
-        }
+        evProgram(root->as.program);
         return 0;
     }
     case PRINT_STMT:
@@ -45,5 +24,40 @@ double ev(Node *root) {
         // Unreachable
         printf("UNRECOGNISED NODE\n");
         return 0;
+    }
+}
+
+double evBinaryOp(BinaryOp *binaryOp) {
+    switch (binaryOp->op) {
+    case '+':
+        return ev(binaryOp->left) + ev(binaryOp->right);
+    case '-':
+        return ev(binaryOp->left) - ev(binaryOp->right);
+    case '/':
+        return ev(binaryOp->left) / ev(binaryOp->right);
+    case '*':
+        return ev(binaryOp->left) * ev(binaryOp->right);
+    default:
+        // Unreachable
+        printf("UNRECOGNISED BINARY OP\n");
+        return 0;
+    }
+}
+
+double evUnaryOp(UnaryOp *unaryOp) {
+    switch (unaryOp->op) {
+    case '-':
+        return -ev(unaryOp->right);
+    default:
+        // Unreachable
+        printf("Unrecognised UNARY OP\n");
+        return 0;
+    }
+}
+
+void evProgram(Program *program) {
+    int len = program->stmts->count;
+    for (int i = 0; i < len; i++) {
+        ev(&program->stmts->nodes[i]);
     }
 }
